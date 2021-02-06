729f19b987e124ed3b9bed123352036607c85c06
Marcin <zolkiewski.marcin@gmail.com>
2021-02-06 19:48:29 +0100

    updated env vars

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const LOG_LEVEL = process.env.LOG_LEVEL as string;
+export const LOG_LEVEL = (process.env.LOG_LEVEL as string) || 'info';
 
 export const DB_URL = process.env.DB_URL as string;
 export const DB_SYNC = process.env.DB_SYNC === 'true';
